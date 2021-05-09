f481a80847cf264173a2bd071cfd99f34bbe280f
OrhanSalman <orhan.salman@gmx.net>
2021-05-09 17:28:20 +0200

    Hot Rebuild ASP.NET Core Web API incl. SQL Server

---
+++ AthenaWebApp/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AthenaWebApp.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string UserMail { get; set; }
+        public string Nickname { get; set; }
+
+//        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
