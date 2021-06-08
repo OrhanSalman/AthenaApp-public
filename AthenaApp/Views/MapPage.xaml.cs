5ec4b79b5aa500cd84672801c5e430d3bbe06715
AlexanderEickelmann <83780575+AlexanderEickelmann@users.noreply.github.com>
2021-06-08 02:40:32 +0200

    manual Location-Track-Button implemented

--- AthenaApp/Views/MapPage.xaml.cs
+++ AthenaApp/Views/MapPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Plugin.Geolocator;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,13 @@ namespace AthenaApp.Views
         public MapPage()
         {
             InitializeComponent();
+        }
+
+        private async void Run_Button(object sender, EventArgs e)
+        {
+            var locator = CrossGeolocator.Current;
+            locator.DesiredAccuracy = 5;
+            var position = await locator.GetPositionAsync();
         }
     }
 }
