5080b59a3bd4a19ec88cc2cf61e5da2d8a0527b5
Orhan <orhan.salman@gmx.net>
2021-07-17 17:53:46 +0200

    bug fixes

--- AthenaWebApp/Controllers/MVC/RoleController.cs
+++ AthenaWebApp/Controllers/MVC/RoleController.cs
@@ -171,19 +171,7 @@ namespace AthenaWebApp.Controllers.MVC
                 return NotFound();
             }
 
-            var roleClaimStringValues = await _context.RoleClaims.Where(c => c.RoleId == id).ToListAsync());
-            var roleClaimBoolValues = new List<IdentityRoleClaim<string>>(
-                foreach(var item in roleClaimBoolValues)
-            {
-
-            });
-            {
-                foreach(var item in roleClaimBoolValues)
-                {
-
-
-                }
-            };
+//            var roleClaimStringValues = await _context.RoleClaims.Where(c => c.RoleId == id).ToListAsync();
 
             return View(await _context.RoleClaims.Where(c => c.RoleId == id).ToListAsync());
 
